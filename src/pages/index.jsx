a3b51fbd7f974ba99b42be077b26f1dfc6120184
shelley cavaness <shelleycavaness@yahoo.com>
2021-02-09 15:58:51 +0100

    changed profile photo for mumu

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -17,7 +17,7 @@ import Projects from '../views/Projects'
 import About from '../views/About'
 import Contact from '../views/Contact'
 
-import avatar from '../images/cavaness.jpg'
+import avatar from '../images/cavaness.png'
 
 const ProjectsWrapper = styled.div`
   ${tw`flex flex-wrap justify-between mt-8`};
